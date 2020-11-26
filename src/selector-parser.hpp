3d21df746218c11ddbc8c94147ce1d98e9fedfea
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-11-26 20:42:47 +0100

    Added docs for functions
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- src/selector-parser.hpp
+++ src/selector-parser.hpp
@@ -3,6 +3,9 @@
 #include <filesystem>
 #include <regex>
 
+/**
+ * A struct that contains all parameters describing a single selector rule.
+ */
 struct SelectorRule
 {
     SelectorRule(
@@ -10,25 +13,53 @@ struct SelectorRule
         std::string regex,
         std::string command,
         std::string entryformat);
-    std::filesystem::path searchdirectory;
-    std::regex regex;
-    std::string command;
-    std::string entryformat;
+    std::filesystem::path searchdirectory; ///< path where the files are located
+    std::regex regex; ///< filter for files in search directory
+    std::string command; ///< a command to be executed for a given file
+    std::string entryformat; ///< a format of entry for a rule in TUI menu
 };
 
+/**
+ * Parser for JSON files describing selector rules.
+ */
 class SelectorParser
 {
     public:
         SelectorParser(std::string jsondir);
 
+        /**
+         * Loads rules from JSON files located in jsondir.
+         *
+         * @param allowfailure if true, the function won't fail on invalid JSON file or entry, but will proceed with other files
+         * @return 0 if executed successfuly
+         */
         int loadRules(bool allowfailure);
+
+        /**
+         * Runs all rules and creates entries from found files.
+         *
+         * @return 0 if executed successfully
+         */
         int findEntries();
+
+        /**
+         * Gets vector of available options.
+         *
+         * @return vector of available options
+         */
         std::vector<std::string>& getOptions();
+
+        /**
+         * Returns command for a given option.
+         *
+         * @param option from getOptions
+         * @return respective command
+         */
         std::string getCommand(std::string option);
     private:
-        std::filesystem::path jsondir;
-        std::vector<SelectorRule> selectorrules;
+        std::filesystem::path jsondir; ///<Path to directory with JSON files describing rules
+        std::vector<SelectorRule> selectorrules; ///<vector of parsed rules
 
-        std::map<std::string, std::string> selectorentries;
-        std::vector<std::string> entrynames;
+        std::map<std::string, std::string> selectorentries; ///<vector of parsed entries
+        std::vector<std::string> entrynames; ///<vector of entry names
 };
--- src/tui-menu.hpp
+++ src/tui-menu.hpp
@@ -7,6 +7,9 @@
 #include <ncurses.h>
 #include <menu.h>
 
+/**
+ * A wrapper for non-standard destructors for unique_ptr
+ */
 template <typename T, int delfunc(T *)>
 struct Destroy
 {
@@ -16,38 +19,78 @@ struct Destroy
     }
 };
 
+/**
+ * Runs all methods required to release MENU structures.
+ */
 int freeMenu(MENU *menu);
 
 using WindowPtr = std::unique_ptr<WINDOW, Destroy<WINDOW, &delwin>>;
 using MenuPtr = std::unique_ptr<MENU, Destroy<MENU, &freeMenu>>;
 
+/**
+ * ncurses-based wrapper for TUI simple menu.
+ */
 class TUISelector
 {
     public:
         TUISelector() {}
 
+        /**
+         * Initializes ncurses and draws windows.
+         */
         void render();
+
+        /**
+         * Releases all temporary ncurses objects.
+         */
         void close();
+
+        /**
+         * Tells if the TUI should be closed.
+         *
+         * @return true if should be closed
+         */
         bool shouldClose();
 
+        /**
+         * Adds new entry to TUI menu
+         *
+         * @param option option name
+         */
         void addOption(const char *option);
 
+        /**
+         * Waits for a keystroke or window resize.
+         *
+         * @param selected reference to a string variable in which the selected entry will be stored
+         * @return true if one of the entries was selected, false otherwise
+         */
         bool waitForAction(std::string &selected);
 
         ~TUISelector();
     private:
-        bool shouldclose = false;
-        std::vector<const char *> options;
-        std::vector<ITEM *> items;
+        bool shouldclose = false; ///<tells if window should be closed
+        std::vector<const char *> options; ///<vector of available options
+        std::vector<ITEM *> items; ///<menu entries
 
-        WindowPtr menuwindow;
-        MenuPtr menu;
+        WindowPtr menuwindow; ///<window holding menu
+        MenuPtr menu; ///<menu object
 
-        WindowPtr helpwindow;
+        WindowPtr helpwindow; ///<window holding help
 
+        /**
+         * Creates a new window object.
+         *
+         * @param x x coordinate in terminal
+         * @param y y coordinate in terminal
+         * @param width width of the window
+         * @param height height of the window
+         * @return unique_ptr to WINDOW object
+         */
         WindowPtr createWindow(int x, int y, int width, int height);
         void removeItems();
 
+        /// help message for the TUI menu
         std::vector<std::string> helpmessage =
             {"Use arrows, PGUP and PGDOWN to move in the view.",
              "Use Enter to select the option.",
