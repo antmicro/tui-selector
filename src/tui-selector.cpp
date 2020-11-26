d431410b0e629c91f8903f976872a4cf67ec84ce
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-11-26 20:25:32 +0100

    Added returning command status
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- src/tui-selector.cpp
+++ src/tui-selector.cpp
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
 
     if (option != "")
     {
-        system(selectorparser.getCommand(option).c_str());
+        return system(selectorparser.getCommand(option).c_str());
     }
 
     return 0;
